b59457ecb0f578f0f0ab0c772f4ab86d7bd3922a
yuxiaojing <240276334@qq.com>
2019-11-01 19:33:22 +0800

    初始化版本

--- src/services/tool/main.go
+++ src/services/tool/main.go
@@ -2,8 +2,52 @@ package main
 
 import (
 	"fmt"
+
+	xlsx "github.com/excelize"
 )
 
 func main() {
 	fmt.Println("usage: excel_tool convert excel to txt.json...format file...")
+
+	writeXlsx()
+	readXlsx()
+}
+
+func writeXlsx() {
+	f := xlsx.NewFile()
+	// Create a new sheet.
+	index := f.NewSheet("Sheet2")
+	// Set value of a cell.
+	f.SetCellValue("Sheet2", "A2", "Hello world.")
+	f.SetCellValue("Sheet1", "B2", 100)
+	// Set active sheet of the workbook.
+	f.SetActiveSheet(index)
+	// Save xlsx file by the given path.
+	err := f.SaveAs("./Book1.xlsx")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func readXlsx() {
+	f, err := xlsx.OpenFile("./Book1.xlsx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Get value from cell by given worksheet name and axis.
+	cell, err := f.GetCellValue("Sheet1", "B2")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(cell)
+	// Get all the rows in the Sheet1.
+	rows, err := f.GetRows("Sheet1")
+	for _, row := range rows {
+		for _, colCell := range row {
+			fmt.Print(colCell, "\t")
+		}
+		fmt.Println()
+	}
 }
